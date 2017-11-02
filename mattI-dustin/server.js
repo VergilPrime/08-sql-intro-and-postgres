f22e2dd74c508d07b08d12b7d409c4a3806ea94a
Matt <wrestler.411@comcast.net>
2017-11-02 10:44:48 -0700

    finished more or less half of the todos and comments

--- mattI-dustin/server.js
+++ mattI-dustin/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// TODO: Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
+// DONE: Install and require the NPM Postgres package 'pg' into your server.js, and ensure that it is then listed as a dependency in your package.json
 
 const fs = require('fs');
 const express = require('express');
@@ -10,20 +10,20 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// TODO: Complete the connection string (conString) for the URL that will connect to your local Postgres database.
+// DONE: Complete the connection string (conString) for the URL that will connect to your local Postgres database.
 
 // Windows and Linux users: You should have retained the user/password from the pre-work for this course.
 // Your OS may require that your conString is composed of additional information including user and password.
-// const conString = 'postgres://USER:PASSWORD@HOST:PORT/DBNAME';
+const conString = 'postgres://postgres:1280@127.0.0.1:5432/postgres';
 
 // Mac:
 // const conString = 'postgres://localhost:5432';
 
 
-// TODO: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
+// DONE: Our pg module has a Client constructor that accepts one argument: the conString we just defined.
 // This is how it knows the URL and, for Windows and Linux users, our username and password for our database when client.connect() is called below. Thus, we need to pass our conString into our pg.Client() call.
 
-const client = new pg.Client('something needs to go here... read the instructions above!');
+const client = new pg.Client(conString);
 
 // REVIEW: Use the client object to connect to our DB.
 client.connect();
@@ -38,7 +38,7 @@ app.use(express.static('./public'));
 // REVIEW: Routes for requesting HTML resources
 app.get('/new', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // This corresponds to 5, because it distributes the cat pictures to Allie, the user.  fetchAll is interacting, as it is also calling loadAll within the same function and requesting/loading data from the database.  The Read part of CRUD is in play here, as it is gathering information and making it readable to the client.
   response.sendFile('new.html', {root: './public'});
 });
 
@@ -46,7 +46,7 @@ app.get('/new', (request, response) => {
 // REVIEW: Routes for making API calls to use CRUD Operations on our database
 app.get('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // This would be on number 3 in the diagram (query).  fetchAll is again being implied here, as it is getting information to use for the results eventually being used in the response.  Read is the part of CRUD implicated here, as it is reading data off of the server, not modifying or changing it.
   client.query('SELECT * FROM articles')
     .then(function(result) {
       response.send(result.rows);
@@ -58,7 +58,7 @@ app.get('/articles', (request, response) => {
 
 app.post('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // Number 3, as it is still requesting to eventually insert data, but it is only a query up to this point.  insertRecord, as it is creating and inserting the requested data into the storage medium.  Create part of CRUD is used here, as it is adding new sets of data that has been requested.
   client.query(
     `INSERT INTO
     articles(title, author, "authorUrl", category, "publishedOn", body)
@@ -83,7 +83,7 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // This is still a query, so it is still number 3 on the diagram.  The method involved is updateRecord, as the inforamtion is being updated within the table.  Update part of CRUD is involved here, as the information is being updated/changed rather than created outright or destroyed outright.
   client.query(
     `UPDATE articles
     SET
@@ -110,7 +110,7 @@ app.put('/articles/:id', (request, response) => {
 
 app.delete('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // Still number 3 on the diagram as it is still querying.  The method involved is deleteRecord, as the server is requesting deletions of data.  The Destroy part of CRUD is involved here, as data is being destroyed.
   client.query(
     `DELETE FROM articles WHERE article_id=$1;`,
     [request.params.id]
@@ -125,7 +125,7 @@ app.delete('/articles/:id', (request, response) => {
 
 app.delete('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // Similarly to the .delete above, the process is still in query and is still number 3.  The method is again deleteRecord, just on a larger scale.  The CRUD application here is Destroy, with more being destroyed at once.
   client.query(
     'DELETE FROM articles;'
   )
@@ -138,7 +138,7 @@ app.delete('/articles', (request, response) => {
 });
 
 // COMMENT: What is this function invocation doing?
-// PUT YOUR RESPONSE HERE
+// Initializing the database by creating tables of data where there are none already. 
 loadDB();
 
 app.listen(PORT, () => {
